f17cfb8ad5d463f1cc7b02c977663f1b3f509f0e
SATYAM SUNDRAM <42437366+satyamsbg@users.noreply.github.com>
2020-08-10 15:01:24 +0530

    Add files via upload

---
+++ Component/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+
+const LoginPage = (props) => {
+  return(
+    <div className="loginpage">
+        <a href="#">You have Sucessfully logged in!</a>
+
+    </div>
+   )
+
+ }
+
+export default LoginPage
